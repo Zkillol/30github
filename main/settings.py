17581c1411d01b8d95013ddb0327591acdd73d58
Zkillol <mtrza.maasaliev.01@gmail.com>
2025-02-10 23:24:20 +0600

    add chat system

--- main/settings.py
+++ main/settings.py
@@ -29,9 +29,7 @@ ALLOWED_HOSTS = []
 
 
 
-AUTH_USER_MODEL = 'registration.User'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -46,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'registration' ,
+
 ]
 
 MIDDLEWARE = [
